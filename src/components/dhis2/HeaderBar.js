801ceb0c0023ef687827d5eb1076c53e5fe306a9
Elisabeth Kirkebø <Elisabeth@Elisabeths-MacBook-Pro-2.local>
2019-09-04 13:23:25 +0530

    Change header bar to new DHIS2 UI library widget

--- src/components/dhis2/HeaderBar.js
+++ src/components/dhis2/HeaderBar.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import HeaderBar from '@dhis2/d2-ui-header-bar';
+//import HeaderBar from '@dhis2/d2-ui-header-bar';
+import { HeaderBar } from '@dhis2/ui-widgets'
+import { DataProvider } from '@dhis2/app-runtime'
+
+
+const baseUrl = process.env.REACT_APP_DHIS2_BASE_URL;
+const apiVersion = 30;
+
 
 export default function(props) {
-    return (<HeaderBar d2={props.d2}  title="Hello"/>)
+    return (
+        <DataProvider baseUrl={baseUrl} apiVersion={apiVersion}>
+            <HeaderBar appName={props.appName}/>
+        </DataProvider>
+    )
+
 }
